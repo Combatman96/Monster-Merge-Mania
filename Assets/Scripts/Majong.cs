96fa23ea56f095b98785b22d5b542642da829b23
ChuongPT_Desktop <phamtienchuong2000@gmail.com>
2023-05-15 17:34:07 +0700

    basic setup

---
+++ Assets/Scripts/Majong.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Majong : MonoBehaviour
+{
+    public SpriteRenderer spriteIcon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Sprite GetMonsterIcon()
+    {
+        return spriteIcon.sprite;
+    }
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    private Sprite monsterIcon;
+    public List<Transform> stackPosList = new List<Transform>();
+
+
+}
